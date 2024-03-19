572585368513b9a80475dfbd2c518a8c84a20261
FearInTheDark <147612664+FearInTheDark@users.noreply.github.com>
2024-03-20 05:29:56 +0700

    Done SVG to PNG

---
+++ src/Converters/SVGToIconConverter.java
@@ -0,0 +1,52 @@
+package Converters;
+
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.TranscodingHints;
+import org.apache.batik.transcoder.image.ImageTranscoder;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.apache.batik.dom.svg.SVGDOMImplementation;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class SVGToIconConverter {
+
+    public static void convertSvgToIcon(String svgFilePath, String outputIconPath, int width, int height) {
+        try {
+            PNGTranscoder transcoder = new PNGTranscoder();
+            TranscodingHints hints = new TranscodingHints();
+            hints.put(PNGTranscoder.KEY_WIDTH, (float) width);
+            hints.put(PNGTranscoder.KEY_HEIGHT, (float) height);
+            transcoder.setTranscodingHints(hints);
+
+            hints.put(ImageTranscoder.KEY_DOM_IMPLEMENTATION, SVGDOMImplementation.getDOMImplementation());
+            hints.put(PNGTranscoder.KEY_DOCUMENT_ELEMENT_NAMESPACE_URI, SVGDOMImplementation.SVG_NAMESPACE_URI);
+            hints.put(PNGTranscoder.KEY_DOCUMENT_ELEMENT, "svg");
+            hints.put(PNGTranscoder.KEY_XML_PARSER_VALIDATING, false);
+
+
+            TranscoderInput input = new TranscoderInput(new File(svgFilePath).toURI().toString());
+            OutputStream outputStream = new FileOutputStream(outputIconPath);
+            TranscoderOutput output = new TranscoderOutput(outputStream);
+            transcoder.transcode(input, output);
+            outputStream.close();
+
+            System.out.println("Biểu tượng đã được tạo thành công.");
+        } catch (Exception e) {
+            System.err.println("Lỗi khi chuyển đổi SVG thành biểu tượng: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        String svgFilePath = "src/icons/frame.svg";
+        String outputIconPath = "src/icons/output.png";
+        int width = 2000;
+        int height = 2000;
+        convertSvgToIcon(svgFilePath, outputIconPath, width, height);
+        File file = new File("src/icons/frame.svg");
+        System.out.println(file.exists() ? "File exists" : "File not found");
+    }
+}
+
