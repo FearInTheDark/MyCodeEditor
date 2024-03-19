bdc162b289558d7f53cf22c1e0720c8426dd3054
FearInTheDark <147612664+FearInTheDark@users.noreply.github.com>
2024-03-19 17:50:02 +0700

    Save Basic Editor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.svg.SVGDocument;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        new ListPath();
+    public static void main(String[] args) {
+
     }
 }
